a3371ba278bb476d0e1a923a69cd7ce0eea8ace9
qiaoyunrui <2546521583@qq.com>
2016-07-22 11:07:56 +0800

    add the logout button

--- app/src/main/java/com/juhezi/salaryquery/query/QueryActivity.java
+++ app/src/main/java/com/juhezi/salaryquery/query/QueryActivity.java
@@ -6,6 +6,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.juhezi.salaryquery.Config;
 import com.juhezi.salaryquery.R;
@@ -16,6 +19,9 @@ import com.juhezi.salaryquery.R;
 public class QueryActivity extends AppCompatActivity {
 
     private QueryFragment mFragment;
+    private QueryPresenter mPresenter;
+
+    private static final String TAG = "QueryActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,5 +47,20 @@ public class QueryActivity extends AppCompatActivity {
             transaction.commit();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.query_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item_query_toolbar_logout) { //退出登录
+            Log.i(TAG, "Logout");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
